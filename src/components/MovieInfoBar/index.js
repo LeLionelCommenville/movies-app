3115260dbc494fa66b17217f7c471376e49d399c
LeLionelCommenville <lionelcommenville@gmail.com>
2022-10-01 13:20:33 +0200

    feat: add proptypes

--- src/components/MovieInfoBar/index.js
+++ src/components/MovieInfoBar/index.js
@@ -1,4 +1,6 @@
 import react from "react";
+import PropTypes from 'prop-types';
+
 // Helpers 
 import { calcTime, convertMoney } from "../../helpers";
 // Styles
@@ -19,6 +21,12 @@ const MovieInfoBar = ({ time, budget, revenue}) => (
         </Content>
     </Wrapper>
 );
+
+MovieInfoBar.propTypes = {
+    time: PropTypes.number,
+    budget: PropTypes.number,
+    revenue: PropTypes.number
+};
 
 export default MovieInfoBar;
 
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef} from "react";
+import PropTypes from 'prop-types';
+
 // Image
 import searchIcon from '../../images/search-icon.svg';
 //Styles 
@@ -34,5 +36,9 @@ const SearchBar = ({ setSearchTerm }) => {
         </Wrapper>
     );
 };
+
+SearchBar.propTypes = {
+    callback: PropTypes.func
+}
 
 export default SearchBar;
